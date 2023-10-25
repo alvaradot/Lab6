d2efa94d6db7eb209dc9ee1832b4fef80508ff5f
Thomas Alvarado <alvaradot@ufl.edu>
2023-10-25 16:30:35 -0400

    s

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-# this is my first github addition
+#Thomas Alvarado
 
-print('hello world')
-
+menu = "Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n"
+option = int(input(menu))
+while option != 3:
+    if option == 1:
+        encode(input("Please enter your password to encode: "))
+    elif option == 2:
+        decode()
+    option = int(input(menu))
+    
