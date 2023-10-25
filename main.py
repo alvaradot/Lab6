e82e422c808b0af60fe49a1e5e63361e00eeef54
Thomas Alvarado <alvaradot@ufl.edu>
2023-10-25 16:33:41 -0400

    s

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #Thomas Alvarado
 
-menu = "Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n"
-option = int(input(menu))
-while option != 3:
-    if option == 1:
-        encode(input("Please enter your password to encode: "))
-    elif option == 2:
-        decode()
+def main():
+    menu = "Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n"
     option = int(input(menu))
-    
+    while option != 3:
+        if option == 1:
+            encode(input("Please enter your password to encode: "))
+        elif option == 2:
+            decode()
+        option = int(input(menu))
+if __name__ == "__main__":
+    main()
